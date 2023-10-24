452ba57a69670564722f7ca06d0eea7e906cf64d
Şeyma <symagn2@gmail.com>
2023-10-24 18:10:07 +0300

    zamlı maaş hesaplama

---
+++ works/dayone/maaszam.py
@@ -0,0 +1,8 @@
+maas = float(input("Lütfen maas giriniz:"))
+
+zamOrani= float(input("Lütfen zam oranını  yüzdesini girin:"))
+
+
+yenimaas = maas*((100+zamOrani)/100)
+
+print("Yeni maasiniz:" + str(yenimaas))
