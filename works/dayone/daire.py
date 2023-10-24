72b2c7ea5bb2eb2e4c9472059392f45d9b1a7cf2
Şeyma <symagn2@gmail.com>
2023-10-24 18:09:45 +0300

    daire hesaplama

---
+++ works/dayone/daire.py
@@ -0,0 +1,10 @@
+yaricap = float(input("Lütfen yaricap girin:"))
+pi = float(input("Lütfen pi sayisi girin:"))
+
+dairecevre = float(2 * yaricap * pi)
+
+print("Dairenin cevresi" + str(dairecevre))
+
+dairealan = float(pi *yaricap * yaricap)
+
+print("Daireni alani : " + str(dairealan))
