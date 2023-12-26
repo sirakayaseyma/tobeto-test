8e2c570c08183a21eb97c44fda7f889f86eef8c6
Şeyma <symagn2@gmail.com>
2023-12-26 14:16:30 +0300

    constants

---
+++ selenium-start/constants/globalConstants.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://www.saucedemo.com"
+invalid_login_xlsx = "data/invalid_login.xlsx"
+USERNAME_ID = "user-name"
+PASSWORD_ID = "password"
+LOGIN_BUTTON = "login-button"
+ERROR_MESSAGE_XPATH = "//*[@id='login_button_container']/div/form/div[3]/h3"
+USERNAME_PASSWORD_DONT_MATCH = "Epic sadface: Username and password do not match any user in this service."
